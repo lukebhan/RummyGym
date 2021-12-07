ba0406e44b9ebfe83833853f5c42eba83bac39f1
Luke Bhan <luke.bhan@vanderbilt.edu>
2021-12-07 16:35:27 -0600

    done with hand

--- src/hand.py
+++ src/hand.py
@@ -1,4 +1,89 @@
 # Hand Class. Utilizes a deck
 # Provides methods for computing the 
-# Tuple (Set, Type, Score)
+# Tuple (Set, Score)
+# where sets is a list of sets and scores is the corresponding value # to each set
 
+from deck import Deck
+
+class Hand(Deck):
+    def getSets(self):
+        numSets = self.getNumSets(self)
+        runSets = self.getRunSets(self)
+        # remove overlapped sets
+        # we prioritize num sets over run sets
+        dictionary = {}
+        for arr in numSets:
+            for item in arr:
+                dictionary[item] = 1
+        indexsToErase = []
+        for idx, arr in enumerate(runSets):
+            for item in arr:
+                if item in dictionary:
+                    indexsToErase.append(idx)
+                    break;
+        # iterate backwards as we want to remove largest indexs first
+        for i in reversed(indexsToErase):
+            runSets.pop(i)
+        return numSets, runSets
+    
+    def getSetScores(self):
+        numSet, runSets = self.getSets()
+        scores = []
+        sets = []
+        for arr in numSet:
+            scores.append(self.getSetScore(arr))
+            sets.append(arr)
+        for arr in runSets:
+            scores.append(self.getSetScore(arr))
+            sets.append(arr)
+        return scores, sets
+
+    @staticmethod
+    def getSetScore(mySet):
+        scoreMap = {'A': 15, 'K': 10, 'Q': 10, 'J': 10, 'T': 10, '9':9, '8':8, '7':7, '6':6, '5':5, '4': 4, '3':3, '2':2}
+        score = 0
+        for item in mySet:
+            score += scoreMap[item[0]]
+        return score
+
+    # Gets all numbered sets. Make this callable without an inst
+    @staticmethod
+    def getNumSets(s):
+        numMap = {12: 'A', 0:'2', 1:'3', 2:'4', 3:'5', 4:'6', 5:'7', 6:'8',  7:'9', 8:'T', 9:'J', 10:'Q', 11:'K'}
+        suiteMap = {0:'Hearts', 1:'Diamonds', 2:'Spades', 3:'Clubs'}
+        mat = s.toNumpy() 
+        # Iterate over numbers
+        totalSets = []
+        for i in range(mat.shape[1]):
+            count = 0
+            curSet = []
+            for j in range(mat.shape[0]):
+                if mat[j][i] == 1:
+                    count += 1
+                    curSet.append(numMap[i]+suiteMap[j])
+            if count >= 3:
+                totalSets.append(curSet)
+        return totalSets
+
+    @staticmethod
+    def getRunSets(s):
+        # not as easy since we need to find when there are three or more ones in a row. 
+        numMap = {0:'2', 1:'3', 2:'4', 3:'5', 4:'6', 5:'7', 6:'8',  7:'9', 8:'T', 9:'J', 10:'Q', 11:'K', 12: 'A'}
+        suiteMap = {0:'Hearts', 1:'Diamonds', 2:'Spades', 3:'Clubs'}
+        mat = s.toNumpy() 
+        totalSets = []
+        for i in range(mat.shape[0]):
+            count = 0
+            curSet = []
+            for j in range(mat.shape[1]):
+                if mat[i][j] == 1:
+                    count += 1
+                    curSet.append(numMap[j] + suiteMap[i])
+                else:
+                    if count >= 3:
+                        totalSets.append(curSet)
+                    count = 0
+                    curSet = []
+            if count >= 3:
+                totalSets.append(curSet)
+        return totalSets
---
+++ test/test_hand.py
@@ -0,0 +1,120 @@
+import unittest
+
+import sys
+sys.path.append("./src")
+from deck import Deck
+from hand import Hand
+
+class TestHandMethods(unittest.TestCase):
+    def testCheckNum(self):
+        d = Deck()
+        d.emptyDeck()
+        d.addCards(["2Clubs", "2Spades", "2Diamonds"])
+        self.assertEqual(len(Hand.getNumSets(d)), 1)
+        for card in ["2Clubs", "2Spades", "2Diamonds"]:
+            self.assertTrue(card in Hand.getNumSets(d)[0])
+        d.addCard("2Hearts")
+        self.assertEqual(len(Hand.getNumSets(d)), 1)
+        for card in ["2Clubs", "2Spades", "2Diamonds"]:
+            self.assertTrue(card in Hand.getNumSets(d)[0])
+        d.removeCard("2Clubs")
+        self.assertEqual(len(Hand.getNumSets(d)), 1)
+        for card in ["2Hearts", "2Spades", "2Diamonds"]:
+            self.assertTrue(card in Hand.getNumSets(d)[0])
+        d.removeCard("2Spades")
+        self.assertEqual(len(Hand.getNumSets(d)), 0)
+        d.addCard("3Spades")
+        self.assertEqual(len(Hand.getNumSets(d)), 0)
+        d.addCards(["QHearts","QSpades", "QDiamonds", "2Clubs", "2Spades"])
+        self.assertEqual(len(Hand.getNumSets(d)), 2)
+        for card in ["2Clubs", "2Spades", "2Diamonds"]:
+            self.assertTrue(card in Hand.getNumSets(d)[0])
+        for card in ["QHearts", "QSpades", "QDiamonds"]:
+            self.assertTrue(card in Hand.getNumSets(d)[1])
+
+    def testCheckRun(self):
+        d = Deck()
+        d.emptyDeck()
+        d.addCards(["2Clubs", "2Spades"])
+        self.assertEqual(len(Hand.getRunSets(d)), 0)
+        d.addCards(["3Spades", "4Clubs", "3Clubs"])
+        self.assertEqual(len(Hand.getRunSets(d)), 1)
+        for card in ["2Clubs",  "3Clubs", "4Clubs"]:
+            self.assertTrue(card in Hand.getRunSets(d)[0])
+        d.removeCard("3Spades")
+        d.addCard("5Clubs")
+        self.assertEqual(len(Hand.getRunSets(d)), 1)
+        for card in ["2Clubs",  "3Clubs", "4Clubs", "5Clubs"]:
+            self.assertTrue(card in Hand.getRunSets(d)[0])
+        d.removeCard("5Clubs")
+        d.addCard("AClubs")
+        self.assertEqual(len(Hand.getRunSets(d)), 1)
+        for card in ["2Clubs",  "3Clubs", "4Clubs"]:
+            self.assertTrue(card in Hand.getRunSets(d)[0])
+        d.addCards(["JClubs", "QClubs", "KClubs"])
+        self.assertEqual(len(Hand.getRunSets(d)), 2)
+        for card in ["2Clubs",  "3Clubs", "4Clubs"]:
+            self.assertTrue(card in Hand.getRunSets(d)[0])
+
+        for card in ["AClubs", "JClubs", "QClubs", "KClubs"]:
+            self.assertTrue(card in Hand.getRunSets(d)[1])
+        d.emptyDeck()
+        d.addCards(["QClubs", "KClubs", "AClubs"])
+        self.assertEqual(len(Hand.getRunSets(d)), 1)
+        for card in ["AClubs", "QClubs", "KClubs"]:
+            self.assertTrue(card in Hand.getRunSets(d)[0])
+
+    def testSetScores(self):
+        d = Deck()
+        d.emptyDeck()
+        d.addCards(["2Clubs", "2Spades"])
+        self.assertEqual(Hand.getSetScore(Hand.getRunSets(d)), 0)
+        d.addCards(["3Spades", "4Clubs", "3Clubs"])
+        self.assertEqual(Hand.getSetScore(Hand.getRunSets(d)[0]), 9)
+        d.removeCard("3Spades")
+        d.addCard("5Clubs")
+        self.assertEqual(Hand.getSetScore(Hand.getRunSets(d)[0]), 14)
+        d.removeCard("5Clubs")
+        # ace is high
+        d.addCard("AClubs")
+        self.assertEqual(Hand.getSetScore(Hand.getRunSets(d)[0]), 9)
+        d.addCards(["JClubs", "QClubs", "KClubs"])
+        self.assertEqual(Hand.getSetScore(Hand.getRunSets(d)[0]), 9)
+        self.assertEqual(Hand.getSetScore(Hand.getRunSets(d)[1]), 45)
+        d.emptyDeck()
+        d.addCards(["QClubs", "KClubs", "AClubs"])
+        self.assertEqual(Hand.getSetScore(Hand.getRunSets(d)[0]), 35)
+
+    def testGetSets(self):
+        h = Hand()
+        numSets, runSets = h.getSets()
+        resArr = [['2Hearts', '2Diamonds', '2Spades', '2Clubs'], ['3Hearts', '3Diamonds', '3Spades', '3Clubs'], ['4Hearts', '4Diamonds', '4Spades', '4Clubs'], ['5Hearts', '5Diamonds', '5Spades', '5Clubs'], ['6Hearts', '6Diamonds', '6Spades', '6Clubs'], ['7Hearts', '7Diamonds', '7Spades', '7Clubs'], ['8Hearts', '8Diamonds', '8Spades', '8Clubs'], ['9Hearts', '9Diamonds', '9Spades', '9Clubs'], ['THearts', 'TDiamonds', 'TSpades', 'TClubs'], ['JHearts', 'JDiamonds', 'JSpades', 'JClubs'], ['QHearts', 'QDiamonds', 'QSpades', 'QClubs'], ['KHearts', 'KDiamonds', 'KSpades', 'KClubs'], ['AHearts', 'ADiamonds', 'ASpades', 'AClubs']]
+        self.assertEqual(sorted(numSets), sorted(resArr))
+        self.assertEqual([], runSets)
+        h = Hand()
+        h.emptyDeck()
+        h.addCards(["2Hearts", "3Hearts", "4Hearts", "6Hearts", "6Clubs", "6Spades"])
+        numSets, runSets = h.getSets()
+        resArr = [["6Hearts", "6Spades", "6Clubs"]]
+        resArr2 = [["2Hearts", "3Hearts", "4Hearts"]]
+        self.assertEqual(sorted(numSets), sorted(resArr))
+        self.assertEqual(sorted(runSets), sorted(resArr2))
+        # Example of a bad case:
+        h.emptyDeck()
+        h.addCards(["2Hearts", "3Hearts", "4Hearts", "5Hearts", "6Hearts", "6Clubs", "6Spades"])
+        numSets, runSets = h.getSets()
+        resArr = [["6Hearts", "6Spades", "6Clubs"]]
+        resArr2 = []
+        self.assertEqual(sorted(numSets), sorted(resArr))
+        self.assertEqual(sorted(runSets), sorted(resArr2))
+
+    def testGetSetsAndScores(self):
+        h = Hand()
+        scores, sets = h.getSetScores()
+        resArr = [['2Hearts', '2Diamonds', '2Spades', '2Clubs'], ['3Hearts', '3Diamonds', '3Spades', '3Clubs'], ['4Hearts', '4Diamonds', '4Spades', '4Clubs'], ['5Hearts', '5Diamonds', '5Spades', '5Clubs'], ['6Hearts', '6Diamonds', '6Spades', '6Clubs'], ['7Hearts', '7Diamonds', '7Spades', '7Clubs'], ['8Hearts', '8Diamonds', '8Spades', '8Clubs'], ['9Hearts', '9Diamonds', '9Spades', '9Clubs'], ['THearts', 'TDiamonds', 'TSpades', 'TClubs'], ['JHearts', 'JDiamonds', 'JSpades', 'JClubs'], ['QHearts', 'QDiamonds', 'QSpades', 'QClubs'], ['KHearts', 'KDiamonds', 'KSpades', 'KClubs'], ['AHearts', 'ADiamonds', 'ASpades', 'AClubs']]
+        self.assertEqual(sorted(sets), sorted(resArr))
+        self.assertEqual(scores, [8, 12, 16, 20, 24, 28, 32, 36, 40, 40, 40, 40, 60])
+
+
+if __name__ == '__main__':
+    unittest.main()
