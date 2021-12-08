b99cbea92f8ef98002b7fd09be40b429d876889f
Alex Feeley <alexandra.feeley.lamb@vanderbilt.edu>
2021-12-08 00:54:08 -0600

    Added torch requirement

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@ from setuptools import setup
 
 setup(name='gym_rummy',
         version='0.0.1', 
-        install_requires=['gym>=0.2.3', 'numpy>=1.20.3'])
+        install_requires=['gym>=0.2.3', 'numpy>=1.20.3', 'torch>=1.9.0'])
